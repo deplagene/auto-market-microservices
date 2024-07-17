8fb100fe5c1349a8ec684c10ac8b7248a5a5028d
deplagene <deplagene@mail.ru>
2024-07-17 17:17:23 +0300

    feat: add test documentation

--- src/users/AutoMarket.Users.API/Controllers/UsersController.cs
+++ src/users/AutoMarket.Users.API/Controllers/UsersController.cs
@@ -15,17 +15,26 @@ public class UserController(
     IMapper mapper) : ControllerBase
 {
     [HttpPost(Name = "createUser")]
+    [ProducesResponseType(typeof(UserDto), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(UserDto), StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<UserDto>> Create(CreateUser.Request request,
         CancellationToken cancellationToken = default) =>
             await sender.Send(request, cancellationToken)
-                .ThenAsync(user => mapper.Map<UserDto>(user.Value));
+                .ThenAsync(user => mapper.Map<UserDto>(user.Value))
+                .ThenAsync(user => CreatedAtAction(
+                    actionName: nameof(GetById),
+                    routeValues: new { id = user.Id },
+                    value: user
+                ));
 
     // [HttpDelete("{id:guid}", Name = "deleteUser")]
     // public async Task Delete(DeleteUser.Request request,
     //     CancellationToken cancellationToken = default) =>
     //         await sender.Send(request, cancellationToken);
 
-    [HttpGet("{id:guid}", Name = "getUser")]
+    [HttpGet(Name = "getUser")]
+    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(UserDto), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<UserDto>> GetById([FromQuery]GetUser.Query query,
         CancellationToken cancellationToken = default) =>
             await sender.Send(query, cancellationToken)
